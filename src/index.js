36ceb6e109f3abe645c4ea0ea0a60f1fc78a229a
Oleksandra <oleksatu@gmail.com>
2021-03-11 21:11:41 +0200

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  var str = str.split("");
+  var arr = JSON.stringify(bracketsConfig).split("");
+  function brackets(arr){
+    var stack = [];
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] == "(" || arr[i] == "[" || arr[i] == "{"){
+        stack.push(arr[i]);
+      }
+      else if(arr[i] == ")"){
+        if(stack[stack.length -1] == "("){
+          stack.pop();
+        }
+        else{
+          return false;
+        }
+      }
+      else if(arr[i] == "]"){
+        if(stack[stack.length -1] == "["){
+          stack.pop();
+        }
+        else{
+          return false;
+        }
+      }
+      else if(arr[i] == "}"){
+        if(stack[stack.length -1] == "{"){
+          stack.pop();
+        }
+        else{
+          return false;
+        }
+      }
+      else if(arr[i] == "|"){
+        if(stack[stack.length -1] == "|"){
+          stack.pop();
+        }
+        else{
+          stack.push(arr[i]);
+        }
+      }
+  }
+        return stack;
+  }
+  return (brackets(str).length != 0 || brackets(arr).length != 0) ? false : true;
 }
